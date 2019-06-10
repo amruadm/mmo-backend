92904bc83d576ad246cd0749eb196f7acc7dda15
Evgeny <a_mru_adm@mail.ru>
2019-06-11 01:07:51 +1000

    check-auth

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,20 +2,64 @@
 
 namespace App\Controller;
 
+use App\Dto\UserCreditinals;
+use App\Dto\UserInfo;
 use App\Repository\UserRepository;
+use JMS\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class UserController extends AbstractController
 {
     /**
      * @Route("/api/v1/user/check-auth", name="user", methods={"POST"})
+     *
+     * @param Request                      $request
+     * @param UserRepository               $userRepository
+     * @param SerializerInterface          $serializer
+     * @param ValidatorInterface           $validator
+     * @param UserPasswordEncoderInterface $passwordEncoder
+     *
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
      */
-    public function checkAuth(Request $request, UserRepository $userRepository)
-    {
+    public function checkAuth(
+        Request $request,
+        UserRepository $userRepository,
+        SerializerInterface $serializer,
+        ValidatorInterface $validator,
+        UserPasswordEncoderInterface $passwordEncoder
+    ) {
+        if ($request->getContentType() !== 'application/json') {
+            throw new BadRequestHttpException();
+        }
 
+        /** @var UserCreditinals $creditinals */
+        $creditinals = $serializer->deserialize($request->getContent(), UserCreditinals::class, 'json');
 
-        return $this->json([]);
+        if (false === $validator->validate($creditinals)) {
+            throw new BadRequestHttpException();
+        }
+
+        $user = $userRepository->findOneBy(['login' => $creditinals->login]);
+        if (null === $user) {
+            throw new NotFoundHttpException();
+        }
+
+        $encodedPassword = $passwordEncoder->encodePassword($user, $creditinals->password);
+
+        if ($user->getPassword() !== $encodedPassword) {
+            throw new NotFoundHttpException();
+        }
+
+        $userInfo = new UserInfo();
+        $userInfo->login = $user->getLogin();
+        $userInfo->id = $user->getId();
+
+        return $this->json($userInfo);
     }
 }
---
+++ src/Dto/UserInfo.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Dto;
+
+/**
+ * Краткая информация об пользователе.
+ */
+class UserInfo {
+    public $id;
+    public $login;
+}
